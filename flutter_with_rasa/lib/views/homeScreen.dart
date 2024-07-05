540f1d3e68115da1ceec94653a8dd0d6d1a588e6
Gopi Shankar Ravady <gopi2605@gmail.com>
2024-07-05 08:31:45 -0500

    integrated socket for displaying current chat users

--- flutter_with_rasa/lib/views/homeScreen.dart
+++ flutter_with_rasa/lib/views/homeScreen.dart
@@ -1,17 +1,46 @@
 // ignore_for_file: prefer_const_constructors, must_be_immutable, prefer_const_literals_to_create_immutables, unused_import, unused_local_variable
 
+import 'package:flutter_with_rasa/views/loginPage.dart';
+
 import '../utils/dbHelper.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
-import '../models/chatUsers.dart';
-import '../models/chatMessages.dart';
+import '../models/socketProvider.dart';
 import '../widgets/conversationList.dart';
 import 'package:word_generator/word_generator.dart';
 import '../views/chatDetailsPage.dart';
+import 'dart:async';
+
+class Conversation {
+  final double conversationId;
+  final List<double> participantIds;
+  final String? lastMessage;
+  final String username;
+  final DateTime? lastMessageTime;
+
+  Conversation({
+    required this.conversationId,
+    required this.participantIds,
+    required this.username,
+    this.lastMessage,
+    this.lastMessageTime,
+  });
+
+  factory Conversation.fromMap(Map<String, dynamic> map) => Conversation(
+    conversationId: map['conversationId'],
+    participantIds: (map['participantIds']as List).cast<double>(),
+    lastMessage: map['lastMessage'],
+    username: map['username'],
+    lastMessageTime: map['lastMessageTime'] != null
+        ? DateTime.parse(map['lastMessageTime'])
+        : null,
+  );
+}
 
 class HomeScreen extends StatefulWidget {
-  int userId;
-  HomeScreen(this.userId, {super.key});
+  double userId;
+  final SocketProvider? _socket;
+  HomeScreen(this.userId, this._socket, {super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -19,21 +48,31 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int itemCount = 0;
-  late Future<List<Map<String, dynamic>>> userData;
+  double receiverId = 0;
+  late Future<List<Conversation>> _conversations;
   
-
   @override
   void initState() {
     super.initState();
-    userData = _fetchUserData();
+    _conversations = _fetchConversations();
   }
 
-Future<List<Map<String, dynamic>>> _fetchUserData() async {
-  List<Map<String, dynamic>> data = await ChatMessages().getLastMessages(widget.userId);
-  return data;
-}
+  Future<List<Conversation>> _fetchConversations() async {
+    Completer<List<Conversation>> completer = Completer<List<Conversation>>();
+    widget._socket?.socket?.emit('get_logged_in_users', widget.userId);
 
-  void _openNewChatPopup(BuildContext context) {
+    widget._socket?.socket?.on('logged_in_users', (data) {
+      List<Conversation> conversations = (data as List)
+          .map((user) => Conversation.fromMap(user))
+          .toList();
+
+      completer.complete(conversations);
+    });
+    List<Conversation> conversations = await completer.future;
+    return conversations;
+  }
+
+  void _openNewChatPopup(BuildContext context, SocketProvider? _socket) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -45,7 +84,7 @@ Future<List<Map<String, dynamic>>> _fetchUserData() async {
           return names;
         }
         List<String> items = randomNames();
-        String? username = items.first;
+        String username = items.first;
         return AlertDialog(
           title: Text('Search'),
           content: SizedBox(
@@ -81,9 +120,9 @@ Future<List<Map<String, dynamic>>> _fetchUserData() async {
             SizedBox(width: 30),
             TextButton(
               onPressed: () async {
+                String contactNumber = '';
                 String generateRandomContactNumber() {
                   Random random = Random();
-                  String contactNumber = '';
                   for (int i = 0; i < 10; i++) {
                     contactNumber += random.nextInt(10).toString();
                   }
@@ -91,27 +130,20 @@ Future<List<Map<String, dynamic>>> _fetchUserData() async {
                 }
                 Random random = Random();
                 int randomInt = random.nextInt(100);
-                final newUser = ChatUsers(
-                  userId: randomInt,
-                  username: username,
-                  contact: generateRandomContactNumber(),
-                );
-
-                await newUser.dbSave();
-
-                final chatMessages = ChatMessages(
-                  senderId: widget.userId,
-                  receiverId: randomInt,
-                  messageContent: '',
-                  timeStamp: DateTime.now(),
-                );
-
-                await chatMessages.dbSave();
-                Navigator.pop(context);
-
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (_) => ChatDetailsPage(widget.userId, randomInt, username as String),
-                ));
+                final newUser = {'username': username, 'password': '', 'contact': contactNumber};
+                _socket?.socket?.emit('create_new_user', newUser);
+                _socket?.socket?.on('create_new_user_response', (response) {
+                  receiverId = response;
+                  if(response.runtimeType == double) {
+                    final mapIds = {'userId': widget.userId, 'receiverId': receiverId};
+                    _socket.socket?.emit('map_conversation', mapIds);
+                    _socket.socket?.on('conversation_id', (response) {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (_) => ChatDetailsPage(widget._socket, widget.userId.toDouble(), receiverId, username, response),
+                      ));
+                    });
+                  }
+                });
               },
               child: Text('Chat'),
             ),
@@ -123,8 +155,8 @@ Future<List<Map<String, dynamic>>> _fetchUserData() async {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Map<String, dynamic>>>(
-        future: userData,
+    return FutureBuilder<List<Conversation>>(
+        future: _conversations,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Scaffold(
@@ -133,7 +165,7 @@ Future<List<Map<String, dynamic>>> _fetchUserData() async {
               ),
             );
           } else {
-            final data = snapshot.data as List<Map<String, dynamic>>;
+            final conversation = snapshot.data as List<Conversation>;
             return Scaffold(
             body: SingleChildScrollView(
               physics: BouncingScrollPhysics(),
@@ -148,7 +180,7 @@ Future<List<Map<String, dynamic>>> _fetchUserData() async {
                         children: <Widget> [
                           Text("Conversations", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
                           GestureDetector(
-                            onTap: () => _openNewChatPopup(context),
+                            onTap: () => _openNewChatPopup(context, widget._socket),
                             child: Container(
                               padding: EdgeInsets.only(left: 8, right: 8,top: 2, bottom: 2),
                               height: 30,
@@ -161,6 +193,29 @@ Future<List<Map<String, dynamic>>> _fetchUserData() async {
                                   Icon(Icons.add,color: Color.fromRGBO(90, 23, 238, 0.4), size: 20,),
                                   SizedBox(width: 2,),
                                   Text("New Chat", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+                                ],
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pushNamedAndRemoveUntil(
+                                '/',
+                                (Route<dynamic> route) => false,
+                              );
+                            },
+                            child: Container(
+                              padding: EdgeInsets.only(left: 8, right: 8,top: 2, bottom: 2),
+                              height: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.purple[50],
+                              ),
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.logout, color: Color.fromRGBO(90, 23, 238, 0.4), size: 20,),
+                                  SizedBox(width: 1,),
+                                  Text("Logout", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
                                 ],
                               ),
                             ),
@@ -189,16 +244,18 @@ Future<List<Map<String, dynamic>>> _fetchUserData() async {
                     ),
                   ),
                   ListView.builder(
-                    itemCount: data.length,
+                    itemCount: conversation.length,
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
-                      final message = data[index];
+                      final message = conversation[index];
                       return ConversationList(
+                        widget._socket,
                         userId: widget.userId,
-                        receiverId: message['receiverId'],
-                        name: message['username'] ?? "",
-                        messageContent: message['messageContent'] ?? "",
-                        timeStamp: DateTime.parse(message['timeStamp'])
+                        receiverId: message.participantIds[1],
+                        messageContent: message.lastMessage,
+                        conversationId: message.conversationId,
+                        name: message.username == "" ? "" : message.username,
+                        timeStamp: message.lastMessageTime
                       );
                     },
                   ),
