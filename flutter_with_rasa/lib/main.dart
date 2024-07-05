728bfaa6861ff84f2d2a989ae71f3ecf360d5c85
Gopi Shankar Ravady <gopi2605@gmail.com>
2024-07-05 07:22:25 -0500

    added general socket connection

--- flutter_with_rasa/lib/main.dart
+++ flutter_with_rasa/lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_with_rasa/models/socketProvider.dart';
 import 'package:flutter_with_rasa/views/loginPage.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Login Page',
-    home: Scaffold(
-      body: LoginPage(),
+const url = "http://192.168.86.239:8080";
+
+void main() async {
+  final socketProvider = SocketProvider();
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login Page',
+      home: Scaffold(
+        body: FutureBuilder(
+          future: socketProvider.connectToServer(url),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return LoginPage(socketProvider);
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
+      )
     )
-  ));
+  );
 }
